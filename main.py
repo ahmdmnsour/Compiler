fb5302fd5fd4b4302ddfbc872f31557ab6cb8b33
Ahmed Mansour <31633069+ahmdmnsour@users.noreply.github.com>
2023-12-21 00:21:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-from parser import Lexer, Parser
 from scanner import Scanner
 
 if __name__ == '__main__':
     file_path = input("Please enter the file path\n")
 
-    tokens = []
-
     sc = Scanner(file_path)
-    tok = sc.get_token()
+    tokens = sc.scan()
 
-    while tok.get_symbol() != -1:
-        tokens.append(tok)
-        tok = sc.get_token()
-        # Lexer.get_next_token(tok)
-
-    lexer = Lexer(tokens)
-    parser = Parser(lexer)
-    parsed_code = parser.parse()
-    print(parsed_code)
-
-    # for tok in tokens:
-    #     print(tok)
+    print(tokens)
