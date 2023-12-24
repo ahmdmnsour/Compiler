d2c103912501864e4b3e94cbbdd6827b0513d122
nagy-ahmed <dawood101dawood@gmail.com>
2023-12-24 17:29:48 +0200

    adding parser

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from scanner import Scanner
+from _parser import Parser
 
 if __name__ == '__main__':
-    file_path = input("Please enter the file path\n")
+    
+    #file_path = input("Please enter the file path\n")
+    file_path="input.txt"
 
     sc = Scanner(file_path)
     tokens = sc.scan()
-
+    print("---------------Tokens---------------")
     print(tokens)
+
+    p=Parser(tokens)
+    parsed_code = p.parse()
+    print("---------------Atoms---------------")
+    print(parsed_code)
